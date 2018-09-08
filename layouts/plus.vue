98d945bf5e7c47c12e8a097c32d5f9eaed296faf
Fordeme <wal24le.mvpt6314@gmail.com>
2018-09-08 22:53:17 +0900

    Eri's Text 2

---
+++ layouts/plus.vue
@@ -0,0 +1,19 @@
+<template> 
+  <el-container> 
+    <el-header>ヘッダー</el-header> 
+    <el-main><nuxt/></el-main> 
+  </el-container> 
+</template> 
+
+<style> 
+
+.el-header, 
+
+.el-footer { 
+  background-color: #b3c0d1; 
+  font-weight: 900; 
+  color: #333; 
+  text-align: center; 
+  line-height: 60px; 
+} 
+</style>
