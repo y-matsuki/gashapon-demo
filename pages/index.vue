28e2d636635979a5668ec7a42cd1be14f0f6e059
Yuto Matsuki <yuto.matsuki@recochoku.co.jp>
2018-09-09 12:44:46 +0900

    update player

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
     <div class="block">
-      <div class="video">
+      <!-- <div class="video">
         <iframe src="https://www.youtube.com/embed/nQHQWuCxXCg?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+      </div> -->
+      <audio id="audio" controls v-if="source_url">
+        <source :src="source_url" type="audio/mp4">
+        Your browser does not support the audio tag.
+      </audio>
+      <el-row>
+        <el-button :plain="true" type="primary" @click="open('eri')">eri</el-button>
+        <el-button :plain="true" type="primary" @click="open('eri2')">eri2</el-button>
+        <el-button :plain="true" type="primary" @click="open('index.mikan')">index.mikan</el-button>
+        <el-button :plain="true" type="primary" @click="open('plus')">plus</el-button>
+        <el-button :plain="true" type="primary" @click="open('sea')">sea</el-button>
+        <el-button :plain="true" type="primary" @click="open('test')">test</el-button>
+      </el-row>
       </div>
       <i class="el-icon-edit"></i>
       <i class="el-icon-share"></i>
@@ -10,8 +23,7 @@
       <el-button type="primary" icon="el-icon-search">Search</el-button>
       <el-table
         :data="tableData"
-        style="width: 100%"
-        :row-class-name="tableRowClassName">
+        style="width: 100%">
         <el-table-column
           prop="date"
           label="Date"
@@ -25,6 +37,11 @@
         <el-table-column
           prop="address"
           label="Address">
+          <template slot-scope="scope">
+            <el-button type="primary" @click="play(scope.row.url)">
+              <i class="el-icon-delete"></i>
+            </el-button>
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -32,9 +49,11 @@
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
   data() {
     return {
+      source_url: null,
       tableData: [{
         date: '2016-05-03',
         name: 'Tom',
@@ -46,12 +65,27 @@ export default {
       }, {
         date: '2016-05-04',
         name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
+        address: 'No. 189, Grove St, Los Angeles',
+        url: 'https://s3-ap-northeast-1.amazonaws.com/gashapon-demo/static/96007906.m4a'
       }, {
         date: '2016-05-01',
         name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
+        address: 'No. 189, Grove St, Los Angeles',
+        url: 'https://s3-ap-northeast-1.amazonaws.com/gashapon-demo/static/96007920.m4a'
       }]
+    }
+  },
+  methods: {
+    open(name) {
+      console.log(name)
+      // this.$router.push(name)
+    },
+    play(url) {
+      console.log(url)
+      this.source_url = url
+      Vue.nextTick(function () {
+        document.getElementById('audio').play()
+      })
     }
   }
 }
@@ -69,5 +103,9 @@ export default {
   right: 0;
   width: 100%;
   height: 100%;
+}
+
+div.el-row {
+  margin: 0.5rem 0;
 }
 </style>
